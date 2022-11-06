9a4d6240657e5b4a953f5ce31db2ae124e8a1d69
GuillaumeDewaghe <55316421+GuillaumeDewaghe@users.noreply.github.com>
2022-11-06 23:05:38 +0100

    Creation of the Talent class with some strings of persona for Survivors

---
+++ IDVRandomizer/Persona/Occurence.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDVRandomizer.Persona
+{
+    public enum Max
+    {
+        One = 1,
+        Three = 3
+    }
+
+    /// <summary>
+    /// Occurence of a talent
+    /// </summary>
+    public class Occurence
+    {
+        /// <summary>
+        /// Current number of points given to the talent
+        /// </summary>
+        public int Current { get; set; }
+        /// <summary>
+        /// Number max of points which can be given to the talent
+        /// </summary>
+        public Max Max { get; set; }
+
+        /// <summary>
+        /// Creates an occurence with a max
+        /// </summary>
+        /// <param name="max"> Max points </param>
+        public Occurence(Max max)
+        {
+            Current = 0;
+            Max = max;
+        }
+
+        public override string ToString()
+        {
+            return "Occurence { Current = " + Current + ", Max = " + (int) Max + " }";
+        }
+    }
+}
---
+++ IDVRandomizer/Persona/Talent.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDVRandomizer.Persona
+{
+    /// <summary>
+    /// Talent from the persona web
+    /// </summary>
+    public class Talent
+    {
+        /// <summary>
+        /// Name of the talent
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Occurence of the talent
+        /// </summary>
+        public Occurence Occurence { get; set; }
+        /// <summary>
+        /// Neighbours of the talent
+        /// </summary>
+        public Neighbours Neighbours { get; set; }
+
+        public Talent()
+        {
+
+        }
+
+        /// <summary>
+        /// Creates a talent with a name and an occurence
+        /// </summary>
+        /// <param name="name"> Name of the talent </param>
+        /// <param name="occurence"> Occurence of the talent </param>
+        public Talent(string name, Occurence occurence)
+        {
+            Name = name;
+            Occurence = occurence;
+            Neighbours = new Neighbours();
+        }
+
+        public override String ToString()
+        {
+            return "Talent { Name = " + Name + ", " + Occurence + ", " + Neighbours + " }";
+        }      
+    }
+}
